e4f2103518dda7cf1c0aeb6dbd4c40941130c5d1
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-07-27 13:08:47 +0100

    Cleanup bounds, window and affine computation
    First compute the bounds as the intersection of all rasters.  As a
    second step, if a mask is being used, compute the intersection of the
    mask and the bounds.
    
    Compute the window that needs to be read for all rasters once this
    computation has been done.
    
    Order the rasters alphabetically (after conversion to lower-case) so
    ensure consistency.

--- evalcontext.py
+++ evalcontext.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import numpy.ma as ma
 import rasterio
@@ -13,8 +15,11 @@ class EvalContext(object):
     self._crop = crop
     self._msgs = True
     self._mask = None
+    self._bounds = None
+    self._affine = None
     self._nodata = -9999
-    self._needed = rasterset.find_needed(what)
+    self._needed = sorted(rasterset.find_needed(what),
+                          key=lambda x: x.lower())
     self._sources = [rasterset[x].source for x in
                      filter(lambda c: rasterset[c].is_raster, self._needed)]
 
@@ -22,16 +27,36 @@ class EvalContext(object):
     # TODO:: scale rasters appropriatelly
     self._res, self._crs = self.check_rasters(self.sources)
     # Compute reading window and affine transform for every raster
-    self._bounds, self._affine, self._shape = self.find_bounds(self.sources)
+    self.bounds = self.find_bounds(self.sources)
 
     # Update bounds, affine, and shape if mask given
     if rasterset.shapes or rasterset.mask:
-      self._bounds, self._affine, \
-        self._shape, self._mask = self.apply_mask(rasterset.shapes,
-                                                  rasterset.mask,
-                                                  rasterset.maskval,
-                                                  rasterset.all_touched)
-      
+      self.bounds, self.mask = self.apply_mask(rasterset.shapes,
+                                               rasterset.mask,
+                                               rasterset.maskval,
+                                               rasterset.all_touched)
+
+    # Set the window we are interested in for all sources
+    for src in self.sources:
+      xl, yh = ~src.reader.affine * (self.bounds[0], self.bounds[1])
+      xh, yl = ~src.reader.affine * (self.bounds[2], self.bounds[3])
+      win = ((math.floor(yl), math.ceil(yh)),
+             (math.floor(xl), math.ceil(xh)))
+      src.window = src.reader.window(*self.bounds)
+      assert src.window == win
+      #src.affine = src.reader.window_transform(src.window)
+
+    # The number of rows and columns must be the same for all rasters.
+    # Trigger and assert if there is more than one window shape in
+    # the raster set.
+    shapes = [window_shape(src.window) for src in self.sources]
+    assert len(set(shapes)) == 1, 'More than one window size'
+
+    # Set the affine transform and shape for the output
+    self._affine = self.sources[0].reader.window_transform(self.sources[0].window)
+    self._shape = window_shape(self.sources[0].window)
+    assert self.shape == self.mask.shape
+    
     # Compute minimal block shape that covers all block shapes
     self._block_shape = self.block_shape(self.sources)
 
@@ -57,6 +82,20 @@ class EvalContext(object):
                                                                    first_res,
                                                                    reader.res))
     return first_res, first_crs
+
+  @staticmethod
+  def find_bounds(sources):
+    bounds = (-180.0, -90.0, 180.0, 90)
+
+    for src in sources:
+      src_bounds = src.reader.bounds
+      if rasterio.coords.disjoint_bounds(bounds, src_bounds):
+        raise ValueError("rasters do not intersect")
+      bounds = (max(bounds[0], src_bounds[0]),
+                max(bounds[1], src_bounds[1]),
+                min(bounds[2], src_bounds[2]),
+                min(bounds[3], src_bounds[3]))
+    return bounds
 
   @staticmethod
   def mask_bounds(shapes):
@@ -91,39 +130,18 @@ class EvalContext(object):
       bounds = (max(minxs), max(minys), min(maxxs), min(maxys))
     else:
       bounds = self.bounds
+
     window = self.sources[0].reader.window(*bounds)
     affine = self.sources[0].reader.window_transform(window)
     shape = window_shape(window)
     if not mask_ds:
-      mask = self.compute_mask(shapes, all_touched,
-                               affine, shape)
+      mask = self.compute_mask(shapes, all_touched, affine, shape)
     else:
       win = mask_ds.window(*bounds)
       data = mask_ds.read(1, masked=True, window=win)
       mask = ma.where(data == maskval, True, False)
-    return bounds, affine, shape, mask
-  
-  @staticmethod
-  def find_bounds(columns):
-    sources = list(map(lambda c: c.reader, columns))
-    first = sources.pop(0)
-    bounds = list(tuple(first.bounds))
+    return bounds, mask
 
-    for src in sources:
-      src_bounds = src.bounds
-      if rasterio.coords.disjoint_bounds(bounds, src_bounds):
-        raise ValueError("rasters do not intersect")
-      bounds[0] = max(bounds[0], src_bounds[0])
-      bounds[1] = max(bounds[1], src_bounds[1])
-      bounds[2] = min(bounds[2], src_bounds[2])
-      bounds[3] = min(bounds[3], src_bounds[3])
-    # The number of rows, columns must be the same for all rasters.
-    # Compute shape based using first raster.
-    bounds = tuple(bounds)
-    window = first.window(*bounds)
-    affine = first.window_transform(window)
-    return bounds, affine, window_shape(window)
-  
   @staticmethod
   def block_shape(sources):
     block_shapes = [s.block_shape for s in sources]
