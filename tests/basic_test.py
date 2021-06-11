3a98e0be9a842d56afff1a410b8eeccd25523b23
Ricardo E. Gonzalez <ricardog@ricardog.com>
2021-06-11 10:38:42 +0100

    Add a test that generates a tree of the expression
    I don't check the layout of the tree and the tree is not very complex,
    but at least it should exercise the main paths of the function.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from pprint import pprint
 from rasterset import Raster, RasterSet, SimpleExpr
 
 dir_path = Path(__file__).parent
@@ -22,4 +23,15 @@ def test_mul():
         'c': SimpleExpr('ice + a * log(b)')
     })
     data, meta = rs.eval('c')
+    return
+
+
+def test_tree():
+    rs = RasterSet({
+        'ice': Raster(Path(dir_path, 'gicew-1700.tif')),
+        'a': SimpleExpr(1),
+        'b': SimpleExpr(2),
+        'c': SimpleExpr('ice + a * log(b)')
+    })
+    pprint(rs.tree('c'))
     return
